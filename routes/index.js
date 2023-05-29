53df79c17c2709c4726748236651f783f3b49dfb
shield0225 <shieldsalcedo@yahoo.com>
2023-05-29 01:15:52 -0400

    updated package.json

--- routes/index.js
+++ routes/index.js
@@ -12,27 +12,27 @@ router.get('/home', function(req, res, next) {
 
 /* GET About Me page. */
 router.get('/aboutme', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
+  res.render('aboutme', { title: 'About Me' });
 });
 
 /* GET Projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'Projects' });
+  res.render('projects', { title: 'Projects' });
 });
 
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
+  res.render('services', { title: 'Services' });
 });
 
 /* GET Education page. */
 router.get('/education', function(req, res, next) {
-  res.render('index', { title: 'Education' });
+  res.render('education', { title: 'Education' });
 });
 
 /* GET Contact Me page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact' });
+  res.render('contact', { title: 'Contact' });
 });
 
 module.exports = router;
